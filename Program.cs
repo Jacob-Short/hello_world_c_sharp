d4c5e503ac90a7af7ad3ccb5597cc20864520542
Jacob-Short <thatmanjshort@gmail.com>
2021-09-06 18:28:03 -0700

    initial lcommit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World from Jacob!");
+
+            Dictionary<String, int> family = new Dictionary<string, int>();
+
+            family.Add("jacob", 25);
+            family.Add("raelynn", 2);
+            family.Add("ayawna", 28);
+            family.Add("samantha", 44);
+            family.Add("stephen", 44);
+            family.Add("loyle", 18);
+            family.Add("emma", 21);
+            family.Add("grace", 19);
+
+            // only pull samanthas value
+            // System.Console.WriteLine(family["samantha"]);
+
+            Console.WriteLine("This is my family:");
+            
+            //iterate throughj each person in family
+            foreach (var person in family)
+            {   
+                System.Console.WriteLine(person);
+
+                // only pull values
+                // System.Console.WriteLine(person.Value);
+            }
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hello_world_c_sharp
+
+## Learning how to create a C# application
+
+## Creating a dictionary with all my family members names along with their age and iterating through them.
